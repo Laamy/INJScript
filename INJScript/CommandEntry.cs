29d954eeee3a6df291075c0d1fc661bc32dcc99f
YeemiRouth <yeemirouth@gmail.com>
2024-05-25 19:01:07 +1000

    example bytecode

--- INJScript/CommandEntry.cs
+++ INJScript/CommandEntry.cs
@@ -11,13 +11,20 @@ public class CommandEntry
         // Define some instructions
         var instructions = new List<Instruction>
         {
-            new Instruction(OpCode.PUSH_INT, 1),         // Push 1
-            new Instruction(OpCode.PUSH_INT, 2),         // Push 2
-            new Instruction(OpCode.ADD),                 // Add top two values
-            new Instruction(OpCode.PUSH_STRING, " is the result"), // Push string " is the result"
-            new Instruction(OpCode.CONCAT),              // Concatenate top two values
-            new Instruction(OpCode.SYS_CALL, 0x10),      // Print the result
-            new Instruction(OpCode.JUMP, 0)              // Jump back to the start
+            // push 1 & 2 onto the stack then add them together (1) with (2)
+            new Instruction(OpCode.PUSH_INT, 1),
+            new Instruction(OpCode.PUSH_INT, 2),
+            new Instruction(OpCode.ADD),
+
+            // push the string of " is the result" and combine the maths from earlier with it
+            new Instruction(OpCode.PUSH_STRING, " is the result"),
+            new Instruction(OpCode.CONCAT),
+
+            // print the current thing ontop of the stack (in this cast it would be "3 is the result"
+            new Instruction(OpCode.SYS_CALL, 0x10),
+
+            // restart loop from bytecode 0 (while loop)
+            new Instruction(OpCode.JUMP, 0)
         };
 
         // Load instructions into the interpreter
